a5353d5ffda8bbf035c3d36d1663827fef216cf8
Phred <adufred7@gmail.com>
2023-10-31 17:19:13 +0000

    added contentful

---
+++ src/constants/content.ts
@@ -0,0 +1,39 @@
+export const content = {
+    testimonials: {
+        image:
+          "https://s3-alpha-sig.figma.com/img/fc02/3707/ec95b73e0797c0837ee904667b1d5b97?Expires=1698624000&Signature=hH~e0GMSada-MyQANm62EZTscOC7~kKenoPCJD7-VPO9ALBC9Cmlab322NxJNSoMoQrIZGI~jRMFWL2j~udNk~FpF~WSGZyQFBrCI2QiQdEBVCOnKzHDp1-5LWfQzgv1fmsNznHBIS6imF89lqdOwPnziKjuYdfXpP1xLmFegESU4ux19FBXKn~fWhsw-6zvEnPLwP~ceHzLbBYwHq5rHzOcHNPKsRI4DhLWvqHoq0yhdsTqjSXzZi5wQFwQi2gspqpFuO9tNsFXoHC~p5O2G1gCXiavzE5OC1r15FgUbOoLZUhTwP3kmhNZ5Xfjs8I8bh6JYWiYBi~C8QsU44WPlg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        title: "We Care With Compassion",
+        subtitle:
+          "Morbi vehicula risus scelerisque justo fermentum volutpat. Praesent lobortis nunc nec ligula elementum, sed accumsan ligula facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra.",
+        left: {
+          title: "What They Say ?",
+          header: "Love To Serve You, With Compassionate Care",
+          subtitle:
+            "Sed congue mollis orci et tempus. Etiam semper elit id turpis ornare posuere a sit amet elit. Nulla facilisi. Nullam augue lectus, lobortis et risus id, ornare porttitor arcu. Pellentesque habitant morbi tristique senectus et netus."
+        },
+        right: [
+          {
+            comment:
+              "Sed congue mollis orci et tempus. Etiam semper elit id turpis ornare posuere a sit amet elit. Nulla facilisi. Nullam augue lectus, lobortis et risus id, ornare porttitor arcu. Pellentesque habitant morbi tristique senectus et netus.",
+            name: "John Doe",
+            position: "CEO, Company",
+            image: "https://i.pravatar.cc/150?u=firstimage"
+          },
+    
+          {
+            comment:
+              "Sed congue mollis orci et tempus. Etiam semper elit id turpis ornare posuere a sit amet elit. Nulla facilisi. Nullam augue lectus, lobortis et risus id, ornare porttitor arcu. Pellentesque habitant morbi tristique senectus et netus.",
+            name: "John Doe",
+            position: "CEO, Company",
+            image: "https://i.pravatar.cc/150?u=secondimage"
+          },
+          {
+            comment:
+              "Sed congue mollis orci et tempus. Etiam semper elit id turpis ornare posuere a sit amet elit. Nulla facilisi. Nullam augue lectus, lobortis et risus id, ornare porttitor arcu. Pellentesque habitant morbi tristique senectus et netus.",
+            name: "John Doe",
+            position: "CEO, Company",
+            image: "https://i.pravatar.cc/150?u=thirdimage"
+          }
+        ]
+      },
+}
