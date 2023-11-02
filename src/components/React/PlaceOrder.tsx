eb7d13a0005e57f4338847913a91de7bbeac144b
Phred <adufred7@gmail.com>
2023-11-02 15:33:36 +0000

    added view transitions

--- src/components/React/PlaceOrder.tsx
+++ src/components/React/PlaceOrder.tsx
@@ -3,11 +3,7 @@ import React from "react";
 export default function Order({ data }: { data: any }) {
   const [quantity, setQuantity] = React.useState(1);
 
-  const {price } = data.fields;
-
-  console.log(data, 'data')
-
-  console.log(quantity, 'quantity')
+  const { price } = data.fields;
 
   return (
     <div className="flex flex-col gap-10">
@@ -31,7 +27,11 @@ export default function Order({ data }: { data: any }) {
       <div className="flex flex-col gap-4">
         <button
           className="bg-pink-300 hover:bg-pink-400 text-white px-5 py-2 rounded-md"
-          onClick={() => window.open(`https://wa.me/233556821729?text=Hi%20Ivy's%20Pastries%20I%20want%20to%20buy%20${data.fields.product}%20x%20${quantity}`)}
+          onClick={() =>
+            window.open(
+              `https://wa.me/233556821729?text=Hi%20Ivy's%20Pastries%20I%20want%20to%20buy%20${data.fields.product}%20x%20${quantity}`
+            )
+          }
         >
           Place Order
         </button>
